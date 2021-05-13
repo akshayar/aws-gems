4cc25b23290982b85a869d1e42268674ecba9c08
Rawat <rawaaksh@147dda0bd89b.ant.amazon.com>
2021-05-13 14:44:29 +0530

    Sample code

--- java-s3-multipart-presigned/src/main/java/com/aksh/test/s3/Main.java
+++ java-s3-multipart-presigned/src/main/java/com/aksh/test/s3/Main.java
@@ -1,16 +1,17 @@
 package com.aksh.test.s3;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
-import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -22,21 +23,30 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 
 public class Main {
+	/** Run with following java arguments - 
+	 * 		<bucket-name> <s3-key> <src-path>
+	 * Example   
+	 *   	abdemo-logs test2/file.pdf src/main/resources/file.pdf
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String args[]) throws Exception {
 		
-		URL url=generatePresignedURL("abdemo-logs","test/slony.pdf");
+		String bucketName=args[0];
+		String s3Key=args[1];
+		String sourceFilePath=args[2];
+		
+		URL url=generatePresignedURL(bucketName,s3Key);
 		
 		System.out.println("Presigned URL:"+url.toURI());
 
 
+		upoadToUrl(url,sourceFilePath);
+	}
+
+	private static void upoadToUrl(URL url, final String sourceFilePath) throws URISyntaxException, IOException, ClientProtocolException {
 		// Creating CloseableHttpClient object
 		CloseableHttpClient httpclient = HttpClients.createDefault();
-
-		// Creating a file object
-		File file = new File("sample.txt");
-
-		// Creating the FileBody object
-		FileBody filebody = new FileBody(file, ContentType.DEFAULT_BINARY);
 
 		// Creating the MultipartEntityBuilder
 		MultipartEntityBuilder entitybuilder = MultipartEntityBuilder.create();
@@ -44,11 +54,9 @@ public class Main {
 		// Setting the mode
 		entitybuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
 
-		// Adding text
-		entitybuilder.addTextBody("sample_text", "This is the text part of our file");
 
 		// Adding a file
-		entitybuilder.addBinaryBody("image", new File("src/main/resources/slony.pdf"));
+		entitybuilder.addBinaryBody("image", new File(sourceFilePath));
 
 		// Building a single entity using the parts
 		HttpEntity mutiPartHttpEntity = entitybuilder.build();
